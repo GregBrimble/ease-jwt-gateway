58e07ef02165b5197414ef35527c4b070da78b32
Greg Brimble <developer@gregbrimble.com>
2021-02-03 16:01:22 +0000

    EASE redirect

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # EASE-JWT-Gateway
+
 An authentication gateway which issues JWT for University of Edinburgh logins (EASE)
+
+## Getting Started
+
+1. SSH into the informatics cluster (e.g. `ssh s1234567@student.ssh.inf.ed.ac.uk`).
+1. `cd /public/homepages/s1234567/web`
+1. `git clone git@github.com:GregBrimble/ease-jwt-gateway.git`
+1. `chmod 755 ease-jwt-gateway`
--- index.php
+++ index.php
@@ -1,14 +1,31 @@
 <?php
 use \Firebase\JWT\JWT;
-
 require_once('./vendor/php-jwt/src/JWT.php');
+
+function redirectToEase() {
+    $user = posix_getpwuid(posix_geteuid())['name'];
+    $path = explode("/public/homepages/" . $user . "/web", __DIR__)[1];
+    header('Location: https://www.ease.ed.ac.uk/cosign.cgi?cosign-eucsCosign-ease.homepages.inf.ed.ac.uk&https://ease.homepages.inf.ed.ac.uk/' . $user . $path);
+    exit;
+}
+
+
+if ($_SERVER["REMOTE_REALM"] !== "EASE.ED.AC.UK" || !isset($_SERVER["REMOTE_USER"])) {
+    redirectToEase();
+}
+
+$timestamp = time();
+$one_minute = 60;
+$one_hour = 60 * $one_minute;
+$ten_hours = 10 * $one_hour;
 
 $key = "example_key";
 $payload = array(
-    "iss" => "http://example.org",
-    "aud" => "http://example.com",
-    "iat" => 1356999524,
-    "nbf" => 1357000000
+    "iss" => "https://www.ease.ed.ac.uk/",
+    "sub" => $_SERVER["REMOTE_USER"],
+    "iat" => $timestamp,
+    "nbf" => $timestamp,
+    "exp" => $timestamp + $ten_hours
 );
 
 $jwt = JWT::encode($payload, $key);
